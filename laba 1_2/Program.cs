820ce3601fe598c7176946764f4d6c869843fbeb
Николай <@DESKTOP-8VK6QD8>
2022-03-16 22:32:18 +0300

    Добавьте файлы проекта.

---
+++ laba 1_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace qwerty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Path = "test-file.txt";
+
+            var writer = new StreamWriter(Path);
+            Console.WriteLine("Введите текст для записи: ");
+            var content = Console.ReadLine();
+            writer.WriteLine(content);
+            writer.Close();
+
+            var reader = new StreamReader(Path);
+            Console.WriteLine($"Файл открыт: {Path}");
+            Console.Write($"Файл содержит: {reader.ReadToEnd()}");
+            reader.Close();
+
+            File.Delete(Path);
+            Console.WriteLine("Файл удалён");
+        }
+    }
+}
